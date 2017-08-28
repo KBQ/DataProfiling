58ae7bcf28b44194f203999076a00b308b39d87f
rifat963 <rifat.abir@gmail.com>
2017-08-28 18:08:42 +0200

    init

---
+++ queries/query_text.R
@@ -0,0 +1,19 @@
+# Schema-level – According to schema, we can
+# compute statistical features such as class / properties
+# usage count, class / properties usage per
+# subject and per object or class / properties hierarchy
+# depth.
+
+
+# 2. Instance-level – Features on this level are computed
+# according to the data only, i.e.,URI usage
+# per subject (/object), triples having a resource
+# (/blanks) as subject (/object), triples with literals,
+# min(/max/avg.) per data type (integer / float /
+#                                 time, etc.), number of internal and external links,
+# number of ingoing (/outgoing) links per instance,
+# number of used languages per literal, classes distribution
+# as subject (/object) per property, property
+# co-occurrence
+
+
